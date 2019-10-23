c569707453a9ae66529d291a22ee8dacb6c6a7c8
lujiancai <770166067@qq.com>
2019-10-23 06:46:46 +0000

    头像上传及显示

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -16,8 +17,15 @@ class UsersController extends Controller
     	return view('users.edit',compact ('user'));
 	}
 
-	public function update(UserRequest $userRequest,User $user){
-    	$user->update($userRequest->all());
+	public function update(UserRequest $userRequest,ImageUploadHandler $imageUploadHandler,User $user){
+    	$data = $userRequest->all();
+		if($userRequest->avatar){
+			$result = $imageUploadHandler->save ($userRequest->avatar,'avatars',$user->id);
+			if($result){
+				$data['avatar'] = $result['path'];
+			}
+		}
+    	$user->update($data);
     	return redirect ()->route('users.show',$user->id)->with('success','个人资料更新成功！');
 	}
 }
