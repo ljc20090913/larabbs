e8e66d9f394740398981fc6cfbdb570953fd2f71
lujiancai <770166067@qq.com>
2019-10-30 06:00:31 +0000

    用户可以删除自己发的话题

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -42,7 +42,7 @@
           <div class="topic-body mt-4 mb-4">
             {!! $topic->body !!}
           </div>
-
+          @can('update',$topic)
           <div class="operate">
             <hr>
             <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
@@ -52,7 +52,7 @@
               <i class="far fa-trash-alt"></i> 删除
             </a>
           </div>
-
+          @endcan
         </div>
       </div>
     </div>
