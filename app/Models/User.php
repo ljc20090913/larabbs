816e2e6d9565750b4edc0a91a8baa1d43d611000
lujiancai <770166067@qq.com>
2019-12-09 07:00:45 +0000

    集成laravel-permission扩展包

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
     use MustVerifyEmailTrait;
+    use HasRoles;
 	use Notifiable {
 		notify as protected laravelNotify;
 	}
