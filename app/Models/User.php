542649675c5327708cd4ef2d7597cb6a0a187d97
lujiancai <770166067@qq.com>
2019-12-10 08:53:30 +0000

    管理员可以管理权限和角色

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
@@ -93,7 +94,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
 	public function setAvatarAttribute($path)
 	{
 		// 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
-		if ( ! \Str::startsWith($path, 'http')) {
+		if ( ! Str::startsWith($path, 'http')) {
 
 			// 拼接完整的 URL
 			$path = config('app.url') . "/uploads/images/avatars/$path";
